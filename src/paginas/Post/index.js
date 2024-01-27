360535e8ef5cdd00d1a901fbaef8dd1c08c848a0
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2024-01-27 13:57:11 -0300

    Rota de Posts com id inexistente

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,28 +1,36 @@
 import "./Post.css";
-import { useParams } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
+import NaoEncontrado from "paginas/NaoEncontrado";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 const Post = () => {
     const params = useParams();
     const post = posts.find(p => p.id === Number(params.id));
 
     if (!post) {
-        return <h1>Post não encontrado...</h1>
+        return <NaoEncontrado />
     }
 
     return (
-        <PostModelo
-            fotoCapa={`/assets/posts/${post.id}/capa.png`}
-            titulo={post.titulo}
-            >
-            <div className="post-markdown-container">
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-        </PostModelo>        
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index element={
+                    <PostModelo
+                        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                        titulo={post.titulo}
+                    >
+                        <div className="post-markdown-container">
+                            <ReactMarkdown>
+                                {post.texto}
+                            </ReactMarkdown>
+                        </div>
+                    </PostModelo>
+                } />
+            </Route>
+        </Routes>
     );
 }
 
